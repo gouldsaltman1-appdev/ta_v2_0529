fdcdc8e11987bc89dac100daa8aa1e6e8849f620
Raghu Betina <raghu@firstdraft.com>
2018-06-02 15:39:54 +0000

    Direct association between equipment and loans

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,8 @@
 class Equipment < ApplicationRecord
   # Direct associations
 
+  has_many   :loans
+
   belongs_to :owner,
              :class_name => "User",
              :counter_cache => :pieces_of_equipment_count
